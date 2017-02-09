e4018e2afc2f1c89bc78e6aed5ae0fd90b89e684
bozhen <bbo78952123456@gmail.com>
2017-02-10 02:19:14 +0800

    module.exports error

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var user = require('./routers/user');
+var app = express();
+
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+   //res.send('Hello World');
+   res.render('pages/index_v1');
+});
+
+app.get('/list_user',function (req, res) {
+	//res.send('index');
+	res.render('pages/index');
+});
+
+app.use('/user', user);
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000')
+});
+
